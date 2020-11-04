6b2d7d5872dbc35e3a7aee34476506ae34cc4235
Andrei Pelinescu-Onciul <andrei@iptel.org>
2020-11-04 20:13:55 +0100

    copyright spelling fix

--- counters.go
+++ counters.go
@@ -1,4 +1,4 @@
-// Copyright 2019-2020 Intuitive Labs Gmbh. All rights reserved.
+// Copyright 2019-2020 Intuitive Labs GmbH. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE.txt file in the root of the source
